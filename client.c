10b0684c0d4d54e82776f81760fd0cd1f8d44a28
Anish Chandgothia <90914492+anish2910@users.noreply.github.com>
2022-08-12 15:05:54 +0530

    Update client.c

--- client.c
+++ client.c
@@ -1,35 +1,117 @@
-#include <netinet/in.h> //structure for storing address information
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/socket.h> //for socket APIs
-#include <sys/types.h>
+#include <unistd.h> 
 
-int main(int argc, char const* argv[])
+#include <sys/socket.h> 
+
+#include<cstring>
+
+#include<sys/types.h>
+
+#include <netinet/in.h> 
+
+#include<arpa/inet.h>
+
+#include<iostream>
+
+using namespace std;
+
+int main() 
+
+{ 
+
+string ipAddress="127.0.0.1";
+
+int port=54000;
+
+	//create socket
+
+	int client=socket(AF_INET,SOCK_STREAM,0);
+
+	if(client==-1)
+
+	{
+
+		cerr<<"cant create";
+
+		return 0;
+
+	}	
+
+//fill sockaddr_in struct
+
+sockaddr_in hint;
+
+hint.sin_family=AF_INET;
+
+hint.sin_port=htons(54000);
+
+inet_pton(AF_INET,ipAddress.c_str(),&hint.sin_addr);	
+
+//connect to server
+
+int con=connect(client, (sockaddr*)&hint ,sizeof(hint));
+
+if(con==-1)
+
 {
-	int sockD = socket(AF_INET, SOCK_STREAM, 0);
 
-	struct sockaddr_in servAddr;
-
-	servAddr.sin_family = AF_INET;
-	servAddr.sin_port
-		= htons(9001); // use some unused port number
-	servAddr.sin_addr.s_addr = INADDR_ANY;
-
-	int connectStatus
-		= connect(sockD, (struct sockaddr*)&servAddr,
-				sizeof(servAddr));
-
-	if (connectStatus == -1) {
-		printf("Error...\n");
-	}
-
-	else {
-		char strData[255];
-
-		recv(sockD, strData, sizeof(strData), 0);
-
-		printf("Message: %s\n", strData);
-	}
+	cerr<<"cant connect";
 
 	return 0;
+
 }
+
+//do while
+
+char buf[4096];
+
+string msg;
+
+do
+
+{
+
+//prompt the user
+
+	cout<<"> ";
+
+	getline(cin,msg);
+
+	if(msg.length()>0)
+
+	{
+
+	//send the msg to our server back
+
+	int sent=send(client,msg.c_str(),msg.size()+1,0);
+
+	if(sent!=-1)
+
+	{
+
+		//wait for server reply
+
+		memset(buf,4096,0);
+
+		int br=recv(client,buf,4096,0);
+
+		if(br>0)
+
+		{
+
+			//print
+
+			cout<<"server>"<<string(buf,0,br)<<endl;
+
+		}
+
+	}
+
+	}
+
+}while(msg.length()>0);
+
+close(client);
+
+return 0; 
+
+} 
